82be1efc209e714439ab70423b5a45af045f5ecf
Serilu <redkirbyog@gmail.com>
2022-08-13 15:24:00 +0200

    input velden pakkettype nieuwe afd opslaan SStrore

--- shiftassistfe/src/components/stations/Afdeling.js
+++ shiftassistfe/src/components/stations/Afdeling.js
@@ -7,40 +7,22 @@ class Afdeling extends React.Component {
 
     state = { 
         totaal: 0, 
-        sampling: 0, 
-        rc: 0, 
-        pallets: 0, 
-        retour: 0,
         pakkettypes: [],
+        types:[],
+        afdeling:"",
         onLoad: true, 
+        test: [],
+        id: 0,
     };
 
-    setValue = event => {
-        switch (event.target.name) {
-            case "sampling":
-                this.setState({sampling: event.target.value});
-                sessionStorage.setItem('{AGT_sampling}', event.target.value);
-                break;
-            case "rc":
-                this.setState({rc: event.target.value});
-                sessionStorage.setItem('AGT_rc', event.target.value);
-                break;
-            case "pallets":
-                this.setState({pallets: event.target.value});
-                sessionStorage.setItem('AGT_pallets', event.target.value);
-                break;
-            case "retour":
-                this.setState({retour: event.target.value});
-                sessionStorage.setItem('AGT_retour', event.target.value);
-                break;
-        };
-        this.setState({totaal: this.state.sampling * 10 + this.state.rc * 10 + this.state.retour * 10 + this.state.pallets * 11}, () => {
-            sessionStorage.setItem('AGT_totaal', this.state.totaal);
-        });
-        
-    }
 
-    getData =()=>{
+
+    setValue = event => {
+
+        sessionStorage.setItem(event.target.id, event.target.value)
+    }
+    
+    getData = () => {
         const afdeling = sessionStorage.getItem("afdeling")
         const afdelingData = {
             afdeling: afdeling,
@@ -49,9 +31,24 @@ class Afdeling extends React.Component {
         axios.post(`api/getPakkettype`, afdelingData).then(response => {
             console.log(response)
             if (response.data.status === 200) {
-                this.setState({pakkettypes: response.data.pakkettype});
-                console.log(this.state.pakkettypes)
+                this.setState({pakkettypes: Object.values(response.data.pakkettype)});
+                
+                // this.setState({afdeling: response.data.pakkettype[0].afdeling})
+                // console.log(this.state.afdeling)
     }});   
+    }
+
+    submit = (event) => {
+        event.preventDefault();
+        let tot = 0;
+
+        for (let i = 0; i < this.state.pakkettypes.length; i++) {
+            tot = tot + Number(sessionStorage.getItem(this.state.pakkettypes[i].pakkettype));
+        }
+        sessionStorage.setItem(sessionStorage.getItem("afdeling") + "_totaal", tot);
+        sessionStorage.setItem(sessionStorage.getItem("afdeling") + "_check", 'gg-check-o');
+        window.location.replace("/home");
+        
     }
 
     render() {
@@ -59,32 +56,34 @@ class Afdeling extends React.Component {
             this.getData();
             this.setState({onLoad:false})
         }
+
+
         return (
+            
             <main className="Station-main">
                 <nav className='navigation'>
                     <a href='/home'><img src={back} className="back" alt="back arrow" /></a>
-                    <h1>{this.props.afdelingNaam}</h1>
+                    <h1>{sessionStorage.getItem("afdeling")}</h1>
                 </nav>
                 <header className='header'>
                     <p>Type</p> <p className='rightp'>Amount</p>
                 </header>
-                <section className='inputLine'>
-                    <label htmlFor="sampling">Sampling</label>
-                    <input type="number" id="sampling" name="sampling" min="0" max="999" placeholder={sessionStorage.getItem('AGT_sampling')} onChange={this.setValue}></input>
-                </section>
-                <section className='inputLine'>
-                    <label htmlFor="rc">RC's gericht</label>
-                    <input type="number" id="rc" name="rc" min="0" max="999" placeholder={sessionStorage.getItem('AGT_rc')} onChange={this.setValue}></input>
-                </section>
-                <section className='inputLine'>
-                    <label htmlFor="pallets">Pallets gericht</label>
-                    <input type="number" id="pallets" name="pallets" min="0" max="999" placeholder={sessionStorage.getItem('AGT_pallets')} onChange={this.setValue}></input>
-                </section>
-                <section className='inputLine'>
-                    <label htmlFor="retour">Retouren UD</label>
-                    <input type="number" id="retour" name="retour" min="0" max="999" placeholder={sessionStorage.getItem('AGT_retour')} onChange={this.setValue}></input>
-                </section>
-                <a className='submitButton' href='/home'><button className='Button'>Submit</button></a>
+
+            <form onSubmit={this.submit}>
+                <article id="pakketten">
+                    {this.state.pakkettypes.map((pakkettype) => {
+
+                        return (
+                            <section className='inputLine'>
+                                <label htmlFor="sampling">{pakkettype.pakkettype}</label>
+                                <input type="number" id={pakkettype.pakkettype} name={pakkettype.pakkettype} min="0" max="999" placeholder={sessionStorage.getItem(pakkettype.pakkettype)} onChange={this.setValue}></input>
+                            </section>
+                        ) 
+                    })}     
+                </article>
+        
+                <button type="submit" className='Button'>Submit</button>
+            </form>
             </main>
         )
 
